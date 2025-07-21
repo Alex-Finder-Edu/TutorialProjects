a833e7f32bce65bf23552d6ac2f0790a64de8240
Alex Finder <alexandru.finder@yahoo.com>
2025-07-21 19:59:43 +0300

    DRF Tutorial Series - 4 Authentication & Permissions: Added Object Level Permissions

--- by_tech_stack/django_rest_framework/tutorial_series/tutorial/snippets/views_class_generics.py
+++ by_tech_stack/django_rest_framework/tutorial_series/tutorial/snippets/views_class_generics.py
@@ -1,6 +1,7 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics, permissions
+from snippets.permissions import IsOwnerOrReadOnly
 from django.contrib.auth.models import User
 
 
@@ -16,7 +17,8 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
 
 
 class UserList(generics.ListAPIView):

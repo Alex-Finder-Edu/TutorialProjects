1917232341dbbfc7a724abb7a133defef1ac124a
Alex Finder <alexandru.finder@yahoo.com>
2025-07-22 21:21:46 +0300

    DRF Tutorial Series - 5 Relationships & Hyperlinked APIs: Added hyperlinks in the API

--- by_tech_stack/django_rest_framework/tutorial_series/tutorial/snippets/urls.py
+++ by_tech_stack/django_rest_framework/tutorial_series/tutorial/snippets/urls.py
@@ -53,4 +53,23 @@ elif views_type == "views_class_generics":
         path('snippets/<int:pk>/highlight/', views_class_generics.SnippetHighlight.as_view()),
     ]
 
+    urlpatterns = format_suffix_patterns([
+    path('', views_class_generics.api_root),
+    path('snippets/',
+        views_class_generics.SnippetList.as_view(),
+        name='snippet-list'),
+    path('snippets/<int:pk>/',
+        views_class_generics.SnippetDetail.as_view(),
+        name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/',
+        views_class_generics.SnippetHighlight.as_view(),
+        name='snippet-highlight'),
+    path('users/',
+        views_class_generics.UserList.as_view(),
+        name='user-list'),
+    path('users/<int:pk>/',
+        views_class_generics.UserDetail.as_view(),
+        name='user-detail')
+])
+
 urlpatterns = format_suffix_patterns(urlpatterns)

af841d37c420d36718902f23e30901357e2968ae
Alex Finder <alexandru.finder@yahoo.com>
2025-07-23 10:25:05 +0300

    DRF Tutorial Series - 6 ViewSets & Routers: Bound ViewSets to URLs in snippets urls.py

--- by_tech_stack/django_rest_framework/tutorial_series/tutorial/snippets/urls.py
+++ by_tech_stack/django_rest_framework/tutorial_series/tutorial/snippets/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from rest_framework import renderers
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views_function_csrf_exempt,\
       views_function_api_view,\
       views_class_api_view,\
       views_class_mixins,\
-      views_class_generics
+      views_class_generics, \
+      views_class_viewsets
 
 '''
 Possible Values for views_type:
@@ -13,9 +15,10 @@ Possible Values for views_type:
     views_class_api_view
     views_class_mixins
     views_class_generics
+    views_class_viewsets
 '''
 
-views_type = "views_class_generics"
+views_type = "views_class_viewsets"
 
 if views_type == "views_function_csrf_exempt":
     urlpatterns = [
@@ -70,4 +73,40 @@ elif views_type == "views_class_generics":
     path('users/<int:pk>/',
         views_class_generics.UserDetail.as_view(),
         name='user-detail')
+])
+
+elif views_type == "views_class_viewsets":
+    from views_class_viewsets import api_root, SnippetViewSet, UserViewSet
+
+    snippet_list = SnippetViewSet.as_view({
+        'get': 'list',
+        'post': 'create'
+    })
+
+    snippet_detail = SnippetViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'patch': 'partial_update',
+        'delete': 'destroy'
+    })
+
+    snippet_highlight = SnippetViewSet.as_view({
+        'get': 'highlight'
+    }, renderer_classes=[renderers.StaticHTMLRenderer])
+
+    user_list = UserViewSet.as_view({
+        'get': 'list'
+    })
+
+    user_detail = UserViewSet.as_view({
+        'get': 'retrieve'
+    })
+
+    urlpatterns = format_suffix_patterns([
+    path('', api_root),
+    path('snippets/', snippet_list, name='snippet-list'),
+    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+    path('users/', user_list, name='user-list'),
+    path('users/<int:pk>/', user_detail, name='user-detail')
 ])

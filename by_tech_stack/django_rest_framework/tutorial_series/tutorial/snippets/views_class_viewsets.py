adecdccbc21f4e633dfe445ffa1b92d1a0734956
Alex Finder <alexandru.finder@yahoo.com>
2025-07-23 10:16:32 +0300

    DRF Tutorial Series - 6 ViewSets & Routers: Added api_root to snippets viewsets

--- by_tech_stack/django_rest_framework/tutorial_series/tutorial/snippets/views_class_viewsets.py
+++ by_tech_stack/django_rest_framework/tutorial_series/tutorial/snippets/views_class_viewsets.py
@@ -2,11 +2,21 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework import renderers
+from rest_framework.decorators import api_view
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
 from snippets.models import Snippet
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):

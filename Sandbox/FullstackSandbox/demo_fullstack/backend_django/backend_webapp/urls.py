60d811111a132f634729b79239834575fae25061
Alex Finder Edu <alex.finder.edu@gmail.com>
2026-02-22 17:56:22 +0200

    FullstackSandbox - Added First Person model and migrations

--- Sandbox/FullstackSandbox/demo_fullstack/backend_django/backend_webapp/urls.py
+++ Sandbox/FullstackSandbox/demo_fullstack/backend_django/backend_webapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('hello-world/', views.hello_wold, name='hello-world'),
+    path('view-models/', views.view_models, name='view-models'),
 ]
--- Sandbox/FullstackSandbox/demo_fullstack/backend_django/backend_webapp/views.py
+++ Sandbox/FullstackSandbox/demo_fullstack/backend_django/backend_webapp/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import HttpResponse
+from backend_webapp.models import Person
 
 # Create your views here.
 def hello_wold(request):
     message: str = "Hello World from bakckend web app"
+    return HttpResponse(message)
+
+def view_models(request):
+    message: str = ""
+    models_count = Person.objects.count()
+    if models_count == 0:
+        message += "<p>No models have been found</p>"
+    else:
+        message += f"<p>{models_count} models have been found</p>"
+        persons = Person.objects.all()
+        for person in persons:
+            message += f"<p>{person}</p>"
     return HttpResponse(message)

32650d48423ce6d84b4cc35c618def016f9623d3
Alex Finder Edu <alex.finder.edu@gmail.com>
2026-03-05 21:57:26 +0200

    FullstackSandbox - Added Address model, migrations and view changes

--- Sandbox/FullstackSandbox/demo_fullstack/backend_django/backend_webapp/models.py
+++ Sandbox/FullstackSandbox/demo_fullstack/backend_django/backend_webapp/models.py
@@ -7,3 +7,15 @@ class Person(models.Model):
 
     def __str__(self):
         return f"ID: {self.id} - {self.first_name} {self.last_name}"
+
+class Address(models.Model):
+    person_id = models.OneToOneField(Person, on_delete=models.CASCADE)
+    street_name = models.CharField(max_length=250)
+    street_number = models.IntegerField()
+    city = models.CharField(max_length=250)
+
+    def __str__(self):
+        p_fname = self.person_id.first_name
+        p_lname = self.person_id.last_name
+        person = f"{p_fname} {p_lname}"
+        return f"{person} Addr - {self.city} {self.street_number} {self.street_name}"

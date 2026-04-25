6155b732ba4b5d1dc2f35ff01b574cde17257d7d
Alex Finder Edu <alex.finder.edu@gmail.com>
2026-04-25 17:47:34 +0300

    Courses - Microsoft CPP - 1

---
+++ courses/microsoft_cpp_fundamentals/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void print_hello()
+{
+    cout << "Hello World!" << endl;
+}
+
+int main() {
+    cout << "Setup Complete!" << endl;
+    print_hello();
+    return 0;
+}
